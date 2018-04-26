6c05d032e0115b58b1b6bb5f46ba8a01e311cc83
hisaichi5518 <hisaichi5518@gmail.com>
2018-04-26 18:36:23 +0900

    Add Tenma::Ichiba::Instance

---
+++ lib/tenma/ichiba/instance.rb
@@ -0,0 +1,63 @@
+require 'itamae/runner'
+
+module Tenma
+  module Ichiba
+    class Instance
+
+      NAME = "remote-build"
+
+      attr_reader :context, :name, :type, :zone, :project, :disk_size
+      def initialize(context)
+        @context = context
+        @name = @context.options.raw.instance_name
+        @type = @context.options.raw.instance_machine_type
+        @zone = @context.options.raw.instance_zone
+        @project = @context.options.raw.instance_project
+        @disk_size = @context.options.raw.instance_disk_size
+      end
+
+      def create
+        puts "Create instance..."
+        puts `gcloud compute instances create #{name} --image-family ubuntu-1710 --image-project ubuntu-os-cloud --preemptible --machine-type #{type} --zone #{zone} --project #{project} --boot-disk-size #{disk_size}GB`
+      end
+
+      def provision
+        file = context.options.raw.ssh_key_file
+
+        if @context.options.create_instance?
+          # Immediately after launching the instance, it takes time to start up, so it can not lead to SSH.
+          puts "sleep 60 sec"
+          sleep 60 # sec
+        end
+
+        # In order to execute Itamae, it is necessary to set up ssh config.
+        puts `gcloud compute config-ssh --remove --ssh-key-file #{file} --project #{project}`
+        puts `gcloud compute config-ssh --ssh-key-file #{file} --project #{project}`
+
+        puts "Provision instance..."
+        role_file = File.expand_path('../itamae/roles/remote.rb', __FILE__)
+        node_json = context.options.raw.node_json
+        Itamae::Runner.run([role_file], :ssh, {
+          host: "#{name}.#{zone}.#{project}",
+          node_json: node_json,
+          color: false,
+          sudo: true,
+          shell: "/bin/sh",
+          log_level: "info",
+        })
+      end
+
+      def delete
+        puts "Delete instance..."
+        puts `gcloud compute instances delete #{name} --delete-disks all --quiet --zone #{zone} --project #{project}`
+      end
+
+      def restart
+        puts "Reset instance..."
+        puts `gcloud compute instances reset #{name} --zone #{zone} --project #{project}`
+        puts "Start instance..."
+        puts `gcloud compute instances start #{name} --zone #{zone} --project #{project}`
+      end
+    end
+  end
+end
