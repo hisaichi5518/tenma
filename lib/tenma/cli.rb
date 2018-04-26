44ca7ba0b5caa3fc1af8c18e268ec081cc5c4adc
hisaichi5518 <hisaichi5518@gmail.com>
2018-04-26 17:41:12 +0900

    Add prepare command

--- lib/tenma/cli.rb
+++ lib/tenma/cli.rb
@@ -1,7 +1,19 @@
-require "thor"
+require 'thor'
+require 'tenma/prepare/command'
 
 module Tenma
   class CLI < ::Thor
 
+    desc "prepare", "Prepare for mobile application releases"
+    option "kpt-issue", type: :boolean
+    option "release-issue", type: :boolean
+    option "release-branch", type: :boolean
+    option "release_pullreqs", type: :boolean
+    option "config-file", type: :string, default: "tenma/prepare.yml"
+    option "github-token", type: :string, default: ENV["TENMA_GITHUB_TOKEN"]
+    option "version", type: :string, required: true
+    def prepare
+      Tenma::Prepare::Command.new(self).execute
+    end
   end
 end
