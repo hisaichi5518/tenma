3a9700d74bf5700f9a1d85e3f4beb43b9a9e9f20
hisaichi5518 <hisaichi5518@gmail.com>
2018-04-26 18:48:59 +0900

    Add ichiba command

--- lib/tenma/cli.rb
+++ lib/tenma/cli.rb
@@ -16,5 +16,21 @@ module Tenma
     def prepare
       Tenma::Prepare::Command.new(self).execute
     end
+
+    desc "ichiba", "instance operation command for Android's remote build"
+    option "create-instance", type: :boolean
+    option "provision-instance", type: :boolean
+    option "delete-instance", type: :boolean
+    option "restart-instance", type: :boolean
+    option "instance-name", type: :string, default: Tenma::Ichiba::Instance::NAME, required: true
+    option "instance-zone", type: :string, default: "asia-northeast1-c", required: true
+    option "instance-project", type: :string, default: ENV["TENMA_ICHIBA_INSTANCE_PROJECT"], required: true
+    option "instance-machine-type", type: :string, default: "n1-highcpu-16", required: true
+    option "instance-disk-size", type: :numeric, default: 20, required: true
+    option "ssh-key-file", type: :string, default: "~/.ssh/id_rsa", required: true
+    option "node-json", type: :string, default: File.expand_path("tenma/ichiba.json"), required: true
+    def ichiba
+      Tenma::Ichiba::Command.new(self).execute
+    end
   end
 end
