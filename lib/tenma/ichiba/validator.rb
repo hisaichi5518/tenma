64c5421a4625ce7f98e9592b96927951b6715e97
hisaichi5518 <hisaichi5518@gmail.com>
2018-04-26 18:30:12 +0900

    Add Tenma::Ichiba::Validator

---
+++ lib/tenma/ichiba/validator.rb
@@ -0,0 +1,31 @@
+module Tenma
+  module Ichiba
+    class Validator
+      attr_reader :context
+      def initialize(context)
+        @context = context
+      end
+
+      def validate!
+        if !command?("gcloud")
+          raise ValidationException, "required gcloud command"
+        end
+
+        if !File.exist?(context.options.raw.node_json)
+          raise ValidationException, "Can't find #{context.options.raw.node_json}"
+        end
+
+        if context.options.provision_instance? && (ENV["USER"].nil? || ENV["USER"].empty?)
+          raise ValidationException, "ENV[USER] is nil or empty..."
+        end
+      end
+
+      def command?(command)
+        system "which #{command} > /dev/null 2>&1"
+      end
+
+      class ValidationException < Exception
+      end
+    end
+  end
+end
